298e47df676dc8a21a0b90f4d09a23905959850d
Alexey <alezgora@gmail.com>
2023-12-22 18:13:49 +0200

    remove useless typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,8 @@
 import { Module } from "@nestjs/common"
-import { ConfigModule, ConfigService } from "@nestjs/config"
+import { ConfigModule } from "@nestjs/config"
 import { CommonModule } from "./realizations/common/module"
-import { TypeOrmModule } from "@nestjs/typeorm"
-
-const ormConfig = require("../ormconfig.js")
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => {
-        ormConfig.url = configService.get("DB_STRING")
-        return ormConfig
-      },
-    }),
-    CommonModule,
-  ],
+  imports: [ConfigModule.forRoot(), CommonModule],
 })
 export class AppModule {}
