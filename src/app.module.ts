ec01b0a64ec9826c28ed64ea9fffd9e97dfcb944
Alexey <alezgora@gmail.com>
2023-12-22 18:28:29 +0200

    import mongoose to app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from "@nestjs/common"
-import { ConfigModule } from "@nestjs/config"
+import { ConfigModule, ConfigService } from "@nestjs/config"
 import { CommonModule } from "./realizations/common/module"
+import { MongooseModule } from "@nestjs/mongoose"
 
 @Module({
-  imports: [ConfigModule.forRoot(), CommonModule],
+  imports: [
+    ConfigModule.forRoot(),
+    CommonModule,
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.getOrThrow("DB_STRING"),
+        useNewUrlParser: true,
+      }),
+    }),
+  ],
 })
 export class AppModule {}
