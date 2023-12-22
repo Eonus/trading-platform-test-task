47aec8b6a8eea54e4a635dcbd6289ec764c29693
Alexey <alezgora@gmail.com>
2023-12-22 20:04:44 +0200

    add transaction and throttler to app

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,19 +2,22 @@ import { Module } from "@nestjs/common"
 import { ConfigModule, ConfigService } from "@nestjs/config"
 import { CommonModule } from "./realizations/common/module"
 import { MongooseModule } from "@nestjs/mongoose"
+import { TransactionModule } from "./realizations/transactions/module"
+import { ThrottlerModule } from "@nestjs/throttler"
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     CommonModule,
+    ThrottlerModule.forRoot(),
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
         uri: configService.getOrThrow("DB_STRING"),
-        useNewUrlParser: true,
       }),
     }),
+    TransactionModule,
   ],
 })
 export class AppModule {}
--- src/realizations/transactions/module.ts
+++ src/realizations/transactions/module.ts
@@ -2,9 +2,11 @@ import { Module } from "@nestjs/common"
 import { MongooseModule } from "@nestjs/mongoose"
 import { Transaction, TransactionSchema } from "./schema"
 import { NestTransactionService } from "./service"
+import { TransactionController } from "./controller"
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: Transaction.name, schema: TransactionSchema }])],
   providers: [NestTransactionService],
+  controllers: [TransactionController],
 })
 export class TransactionModule {}
